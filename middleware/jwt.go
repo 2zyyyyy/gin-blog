fad4d510b5be2f3b6cf3231c6f1f39731483b34e
wanli <zhangyun@ifugle.com>
2022-08-17 19:48:37 +0800

    add:修改路径

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,7 +1,7 @@
 package middleware
 
 import (
-	"gin-blog/utils"
+	res "gin-blog/utils"
 	e "gin-blog/utils/errmsg"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-var JwtKey = []byte(utils.JWT.SigningKey)
+var JwtKey = []byte(res.JWT.SigningKey)
 
 const (
 	AuthorizationKey = "Authorization"
@@ -26,12 +26,12 @@ type MyClaims struct {
 
 // SetToken 生成token
 func SetToken(username string) (string, e.ResCode) {
-	expireTime := utils.JWT.ExpiresTime
+	expireTime := res.JWT.ExpiresTime
 	SetClaims := MyClaims{
 		Username: username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expireTime,
-			Issuer:    utils.JWT.Issuer,
+			Issuer:    res.JWT.Issuer,
 		},
 	}
 	reqClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, SetClaims)
@@ -61,27 +61,27 @@ func JwtMiddleware() gin.HandlerFunc {
 		tokenHeader := ctx.Request.Header.Get(AuthorizationKey)
 		if tokenHeader == Null {
 			code = e.ErrorTokenExist
-			utils.ResponseErrorWithMsg(ctx, code, code.GetMsg())
+			res.ResponseErrorWithMsg(ctx, code, code.GetMsg())
 			ctx.Abort()
 			return
 		}
 		checkToken := strings.SplitN(tokenHeader, SpaceKey, Num)
 		if len(checkToken) != 2 && checkToken[0] != BearerKey {
 			code = e.ErrorTokenTypeWrong
-			utils.ResponseErrorWithMsg(ctx, code, code.GetMsg())
+			res.ResponseErrorWithMsg(ctx, code, code.GetMsg())
 			ctx.Abort()
 			return
 		}
 		key, checkCode := CheckToken(checkToken[1])
 		if checkCode == e.ERROR {
 			code = e.ErrorTokenWrong
-			utils.ResponseErrorWithMsg(ctx, code, code.GetMsg())
+			res.ResponseErrorWithMsg(ctx, code, code.GetMsg())
 			ctx.Abort()
 			return
 		}
 		if time.Now().Unix() > key.ExpiresAt {
 			code = e.ErrorTokenRuntime
-			utils.ResponseErrorWithMsg(ctx, code, code.GetMsg())
+			res.ResponseErrorWithMsg(ctx, code, code.GetMsg())
 			ctx.Abort()
 			return
 		}
